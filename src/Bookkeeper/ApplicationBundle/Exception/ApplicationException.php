168de06337fe6c8ca050030dfc9fe63d84c28ec9
E.G. ismaail <contact@ismaail.com>
2015-04-12 18:48:38 +0100

    Create ApplicationException class

---
+++ src/Bookkeeper/ApplicationBundle/Exception/ApplicationException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Bookkeeper\ApplicationBundle\Exception;
+
+use Exception;
+
+/**
+ * Class ApplicationException
+ * @package Bookkeeper\ApplicationBundle
+ */
+class ApplicationException extends Exception
+{
+}
