c9427cad6c4773deb175626131c1f235ccf67c7e
E.G. ismaail <contact@ismaail.com>
2015-04-12 19:32:51 +0100

    Add UnitTest for throwing ApplicationException if email params not defined

--- src/Bookkeeper/ApplicationBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Bookkeeper/ApplicationBundle/Tests/Controller/DefaultControllerTest.php
@@ -3,6 +3,7 @@
 namespace Bookkeeper\ApplicationBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Bookkeeper\ApplicationBundle\Controller\DefaultController;
 
 /**
  * Class DefaultControllerTest
@@ -19,5 +20,77 @@ class DefaultControllerTest extends WebTestCase
         $this->assertTrue($client->getResponse()->isSuccessful(), 'Request to "/" was not successful');
 
         $this->assertEquals('text/html; charset=UTF-8', $client->getResponse()->headers->get('Content-Type'));
+    }
+
+    /**
+     * @expectedException \Bookkeeper\ApplicationBundle\Exception\ApplicationException
+     */
+    public function testEmailSendMessageThrowsExceptionIfSenderParamsNotDefined()
+    {
+        $containerMock = $this->getMockBuilder('appDevDebugProjectContainer')
+            ->setMethods(array('getParameter', 'get'))
+            ->getMock();
+
+        $containerMock->expects($this->any())
+            ->method('getParameter')
+            ->will($this->returnValue(array())); // all email sender params not defined
+
+        $object             = new DefaultController();
+        $reflectionClass    = new \ReflectionClass($object);
+        $reflectionProperty = $reflectionClass->getProperty('container');
+        $reflectionProperty->setAccessible(true);
+        $reflectionProperty->setValue($object, $containerMock);
+
+        $method = new \ReflectionMethod('Bookkeeper\ApplicationBundle\Controller\DefaultController', 'sendMessage');
+        $method->setAccessible(true);
+        $method->invoke($object, 'subject test', 'message body', 'test@test.com');
+    }
+
+    /**
+     * @expectedException \Bookkeeper\ApplicationBundle\Exception\ApplicationException
+     */
+    public function testEmailSendMessageThrowsExceptionIfSenderNameParamsNotDefined()
+    {
+        $containerMock = $this->getMockBuilder('appDevDebugProjectContainer')
+            ->setMethods(array('getParameter', 'get'))
+            ->getMock();
+
+        $containerMock->expects($this->any())
+            ->method('getParameter')
+            ->will($this->returnValue(array('address' => 'foo@example.com'))); // email sender name param not defined
+
+        $object             = new DefaultController();
+        $reflectionClass    = new \ReflectionClass($object);
+        $reflectionProperty = $reflectionClass->getProperty('container');
+        $reflectionProperty->setAccessible(true);
+        $reflectionProperty->setValue($object, $containerMock);
+
+        $method = new \ReflectionMethod('Bookkeeper\ApplicationBundle\Controller\DefaultController', 'sendMessage');
+        $method->setAccessible(true);
+        $method->invoke($object, 'subject test', 'message body', 'test@test.com');
+    }
+
+    /**
+     * @expectedException \Bookkeeper\ApplicationBundle\Exception\ApplicationException
+     */
+    public function testEmailSendMessageThrowsExceptionIfSenderAddressParamsNotDefined()
+    {
+        $containerMock = $this->getMockBuilder('appDevDebugProjectContainer')
+            ->setMethods(array('getParameter', 'get'))
+            ->getMock();
+
+        $containerMock->expects($this->any())
+            ->method('getParameter')
+            ->will($this->returnValue(array('name' => 'foobar'))); // email sender address param not defined
+
+        $object             = new DefaultController();
+        $reflectionClass    = new \ReflectionClass($object);
+        $reflectionProperty = $reflectionClass->getProperty('container');
+        $reflectionProperty->setAccessible(true);
+        $reflectionProperty->setValue($object, $containerMock);
+
+        $method = new \ReflectionMethod('Bookkeeper\ApplicationBundle\Controller\DefaultController', 'sendMessage');
+        $method->setAccessible(true);
+        $method->invoke($object, 'subject test', 'message body', 'test@test.com');
     }
 }
