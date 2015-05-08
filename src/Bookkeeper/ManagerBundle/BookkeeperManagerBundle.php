0b3ec014d8194182e48da72683cfb21581a4cc12
E.G. ismaail <contact@ismaail.com>
2015-05-08 01:57:20 +0100

    Add DocBlocks

--- src/Bookkeeper/ManagerBundle/BookkeeperManagerBundle.php
+++ src/Bookkeeper/ManagerBundle/BookkeeperManagerBundle.php
@@ -4,6 +4,10 @@ namespace Bookkeeper\ManagerBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Class BookkeeperManagerBundle
+ * @package Bookkeeper\ManagerBundle
+ */
 class BookkeeperManagerBundle extends Bundle
 {
 }
--- src/Bookkeeper/UserBundle/BookkeeperUserBundle.php
+++ src/Bookkeeper/UserBundle/BookkeeperUserBundle.php
@@ -4,6 +4,10 @@ namespace Bookkeeper\UserBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Class BookkeeperUserBundle
+ * @package Bookkeeper\UserBundle
+ */
 class BookkeeperUserBundle extends Bundle
 {
 }
