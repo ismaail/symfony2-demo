3b18deb6e004ee0c74b0aa5663ec5e9d25d12f4d
E.G. ismaail <contact@ismaail.com>
2015-05-27 03:16:06 +0100

    Update login mock to set Role from the $role param

--- src/Bookkeeper/ManagerBundle/Tests/Controller/BookControllerTest.php
+++ src/Bookkeeper/ManagerBundle/Tests/Controller/BookControllerTest.php
@@ -27,7 +27,7 @@ class BookControllerTest extends WebTestCase
 
     public function testNewActionIsAccessibleByRoleAdmin()
     {
-        $this->logIn();
+        $this->logIn(EntityUser::ROLE_ADMIN);
 
         $this->client->request('GET', '/new');
 
@@ -46,7 +46,7 @@ class BookControllerTest extends WebTestCase
 
     public function testCreateActionIsAccessibleByRoleAdmin()
     {
-        $this->logIn();
+        $this->logIn(EntityUser::ROLE_ADMIN);
 
         $this->client->request('POST', '/create');
 
@@ -65,7 +65,7 @@ class BookControllerTest extends WebTestCase
 
     public function testEditActionIsAccessibleByRoleAdmin()
     {
-        $this->logIn();
+        $this->logIn(EntityUser::ROLE_ADMIN);
         $this->mockBookModelGetBookBySlug('book-title');
 
         $this->client->request('GET', '/edit/book-title');
@@ -85,7 +85,7 @@ class BookControllerTest extends WebTestCase
 
     public function testUpdateActionIsAccessibleByRoleAdmin()
     {
-        $this->logIn();
+        $this->logIn(EntityUser::ROLE_ADMIN);
         $this->mockBookModelGetBookBySlug('book-title');
 
         $this->client->request('PUT', '/update/book-title');
@@ -105,7 +105,7 @@ class BookControllerTest extends WebTestCase
 
     public function testDeleteActionIsAccessibleByRoleAdmin()
     {
-        $this->logIn();
+        $this->logIn(EntityUser::ROLE_ADMIN);
         $this->mockBookModelGetBookBySlug('book-title');
 
         $this->client->request('DELETE', '/delete/book-title');
@@ -124,7 +124,12 @@ class BookControllerTest extends WebTestCase
         $this->assertStringEndsWith('/login', $this->client->getHistory()->current()->getUri());
     }
 
-    public function mockBookkeeperEntityUser()
+    /**
+     * @param string $role
+     *
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    public function mockBookkeeperEntityUser($role)
     {
         $mockUser = $this
             ->getMockBuilder('\Bookkeeper\UserBundle\Entity\User')
@@ -135,18 +140,21 @@ class BookControllerTest extends WebTestCase
         $mockUser
             ->expects($this->atLeastOnce())
             ->method('serialize')
-            ->will($this->returnValue('a:3:{i:0;i:1;i:1;s:5:"admin";i:2;s:10:"ROLE_ADMIN";}'));
+            ->will($this->returnValue(sprintf('a:3:{i:0;i:1;i:1;s:4:"user";i:2;s:10:"%s";}', $role)));
 
         $reflectionClass = new \ReflectionClass('\Bookkeeper\UserBundle\Entity\User');
+        // id property
         $reflectionIdProperty = $reflectionClass->getProperty('id');
         $reflectionIdProperty->setAccessible(true);
         $reflectionIdProperty->setValue($mockUser, 1);
+        // username property
         $reflectionUsernameProperty = $reflectionClass->getProperty('username');
         $reflectionUsernameProperty->setAccessible(true);
-        $reflectionUsernameProperty->setValue($mockUser, 'admin');
+        $reflectionUsernameProperty->setValue($mockUser, 'user');
+        // roles property
         $reflectionRolesProperty = $reflectionClass->getProperty('roles');
         $reflectionRolesProperty->setAccessible(true);
-        $reflectionRolesProperty->setValue($mockUser, EntityUser::ROLE_ADMIN);
+        $reflectionRolesProperty->setValue($mockUser, $role);
 
         return $mockUser;
     }
@@ -167,18 +175,20 @@ class BookControllerTest extends WebTestCase
     }
 
     /**
-     * Mock loggedIn user with role "ROLE_ADMIN"
+     * @param string $role
+     *
+     * Mock loggedIn user with role
      */
-    private function logIn()
+    private function logIn($role)
     {
-        $user = $this->mockBookkeeperEntityUser();
+        $user = $this->mockBookkeeperEntityUser($role);
         $this->mockEntityUserProvider($user);
 
         /** @var \Symfony\Component\HttpFoundation\Session\Session $session */
         $session = $this->client->getContainer()->get('session');
 
         $firewall = 'secured_area';
-        $token    = new UsernamePasswordToken($user, null, $firewall, array(EntityUser::ROLE_ADMIN));
+        $token    = new UsernamePasswordToken($user, null, $firewall, array($role));
         $session->set('_security_' . $firewall, serialize($token));
         $session->save();
 
