8f49b2bfee40c56d5c803a5c62e9e21858ca6e1d
E.G. ismaail <contact@ismaail.com>
2015-05-27 01:54:35 +0100

    Add access testes for "login" & "logout" paths

--- src/Bookkeeper/UserBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Bookkeeper/UserBundle/Tests/Controller/DefaultControllerTest.php
@@ -3,6 +3,9 @@
 namespace Bookkeeper\UserBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\BrowserKit\Cookie;
+use Bookkeeper\UserBundle\Entity;
 
 /**
  * Class DefaultControllerTest
@@ -10,17 +13,65 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
  */
 class DefaultControllerTest extends WebTestCase
 {
-    public function testLoginAction()
-    {
-        $client = static::createClient();
+    /**
+     * @var \Symfony\Bundle\FrameworkBundle\Client
+     */
+    public $client;
 
-        $client->request('GET', '/login');
+    public function setUp()
+    {
+        $this->client = static::createClient();
     }
 
-    public function testLogoutAction()
+    /**
+     * Mock loggedIn user with role "ROLE_ADMIN"
+     */
+    private function logIn()
     {
-        $client = static::createClient();
+        /** @var \Symfony\Component\HttpFoundation\Session\Session $session */
+        $session = $this->client->getContainer()->get('session');
 
-        $client->request('GET', '/logout');
+        $firewall = 'secured_area';
+        $token    = new UsernamePasswordToken('user', null, $firewall, array(Entity\User::ROLE_ADMIN));
+        $session->set('_security_' . $firewall, serialize($token));
+        $session->save();
+
+        $cookie = new Cookie($session->getName(), $session->getId());
+        $this->client->getCookieJar()->set($cookie);
+    }
+
+    public function testLoginPathIsAccessibleByAnonymousUser()
+    {
+        $this->client->request('GET', '/login');
+
+        $this->assertTrue($this->client->getResponse()->isSuccessful());
+    }
+
+    public function testLoginPathIsRedirectedForLoggedInUser()
+    {
+        $this->logIn();
+
+        $this->client->request('GET', '/login');
+
+        $this->assertTrue(
+            $this->client->getResponse()->isRedirect(),
+            "GET request to /login is not redirected if user is logged-in"
+        );
+    }
+
+    public function testLogoutPathIsRedirectedForAnonymousUser()
+    {
+        $this->client->request('GET', '/logout');
+
+        $this->assertTrue($this->client->getResponse()->isRedirect());
+    }
+
+    public function testLogoutPathIsRedirectedForLoggedInUser()
+    {
+        $this->logIn();
+
+        $this->client->request('GET', '/logout');
+
+        $this->assertTrue($this->client->getResponse()->isRedirect());
     }
 }
