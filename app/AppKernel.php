7d5011643238e2c11e2b08561acb99c75777efda
E.G. ismaail <contact@ismaail.com>
2015-09-07 01:14:17 +0100

    Add & configure Sonata Admin

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,12 @@ class AppKernel extends Kernel
             new Bookkeeper\ApplicationBundle\BookkeeperApplicationBundle(),
             new Bookkeeper\ManagerBundle\BookkeeperManagerBundle(),
             new Bookkeeper\UserBundle\BookkeeperUserBundle(),
+
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
