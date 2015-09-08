192e7839ba0224cf8be348fe885d7e8a1553862d
E.G. ismaail <contact@ismaail.com>
2015-09-08 01:12:57 +0100

    DocBlock Fixes

--- src/Bookkeeper/AdminBundle/BookkeeperAdminBundle.php
+++ src/Bookkeeper/AdminBundle/BookkeeperAdminBundle.php
@@ -4,6 +4,10 @@ namespace Bookkeeper\AdminBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Class BookkeeperAdminBundle
+ * @package Bookkeeper\AdminBundle
+ */
 class BookkeeperAdminBundle extends Bundle
 {
 }
